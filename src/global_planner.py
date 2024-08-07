abc513bc0c242a74960ba327a7ab43e5405f5e0f
jinooLi <jinu1423@naver.com>
2024-08-07 22:51:38 +0900

    커브 방향 판단까지 짬

--- src/global_planner.py
+++ src/global_planner.py
@@ -1,16 +1,135 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-# 맵에서 격자를 생성하는 코드를 작성해야함.
-# 이때 맵은 0또는 1을 가지는 2차원 배열이다.
-# 1제곱미터당 맵의 배열 크기는 100x100이다.
-# 우선, 맵은 직사각형 두개가 같은 중심을 가지고 다른 크기를 가지는 형태로 생성한다.
+class map_line_data:
+    def __init__(self, leftline:np, rightline:np):
+        self.leftline = leftline
+        self.rightline = rightline
 
-# 1. 맵 데이터가 있으면 그것을 격자로 잘라야한다. 
-# 1.1. 우선 격자를 만들기 전에 물체의 진행방향에 수직인 직선들을 그어야한다. 
-# 1.1.1. 트랙의 왼쪽 선과 오른쪽 선중 국소적으로 더 긴 쪽을 선택한다.
-# 1.1.1.1 먼저 잡았던 직선의 끝점을 
-# 1.1.2. 더 긴 쪽의 트랙을 일정한 간격으로 나누어 점을 찍는다. 
-# 1.1.3. 그 점과 반대쪽 트랙의 점 중 가장 가까운 점과 직선을 긋는다. 
-# 1.1.4. 
-# 1.2. 그 다음 그 직선들을 적당히 잘라서 격자를 만든다.
+    # 네모 맵 생성
+    def __init__(self):
+        right= np.array([[0, 0], [1000, 0], [1000, 1000], [0, 1000], [0, 0]], dtype=np.float32)
+        left= np.array([[100, 100], [900, 100], [900, 900], [100, 900], [100, 100]], dtype=np.float32)
+
+        self.leftline = np.array([left[0]], dtype=np.float32)
+        self.rightline = np.array([right[0]], dtype=np.float32)
+
+        for i in range(len(left)-1):
+            dot_num = int(np.linalg.norm(left[i+1] - left[i]))*10
+            for j in range(dot_num):
+                self.leftline = np.append(self.leftline, [left[i] + (left[i+1] - left[i]) * j/dot_num], axis=0)
+
+        for i in range(len(right)-1):
+            dot_num = int(np.linalg.norm(right[i+1] - right[i]))*10
+            for j in range(dot_num):
+                self.rightline = np.append(self.rightline, [right[i] + (right[i+1] - right[i]) * j/dot_num], axis=0)
+
+        self.leftline = np.delete(self.leftline, 0, axis=0)
+        self.rightline = np.delete(self.rightline, 0, axis=0)
+
+        print(self.leftline)
+        print(self.rightline)
+
+        self.left_index = len(self.leftline)
+        self.right_index = len(self.rightline)
+    
+    def get_left_line(self)->np:
+        return self.leftline
+    
+    def get_right_line(self)->np:
+        return self.rightline
+
+    def get_left_circuler_index(self, index:int)->int:
+        return index % self.left_index
+    
+    def get_right_circuler_index(self, index:int)->int:
+        return index % self.right_index
+        
+
+class grid_node_map:
+    def __init__(self, map_data:map_line_data, line_div_num:int = 5, line_dist_index:int = 100):
+        self.map_data = map_data
+        self.line_div_num = line_div_num
+        self.line_dist_index = line_dist_index
+
+    def get_grid_node(self):
+        grid_node = np.zeros((1, self.line_div_num), dtype=np.float32)
+        pass
+
+    def get_grid_line(self, index_dist:int)->np:
+        left_line = self.map_data.get_left_line()
+        right_line = self.map_data.get_right_line()
+
+        # 처음 수직선을 그릴 기준점을 정한다.
+        # 이때, 오른쪽 커브인지 외쪽 커브인지 판단한다.
+        # 오른쪽 커브인 경우, 왼쪽 점을 기준으로 수직선을 그린다. 반대는 오른쪽 점으로 한다.
+        if self.is_left_curve(left_index = 0, right_index = 0):
+            pass
+        else:
+            pass
+
+    # index번째 점을 기준으로 detect_range만큼의 점을 이용하여 왼쪽 커브인지 오른쪽 커브인지 판단
+    def is_left_curve(self, left_index:int, right_index:int, detect_range:int = 4)->bool:
+        left_line = self.map_data.get_left_line()
+        right_line = self.map_data.get_right_line()
+
+        # find start point
+        lstart_point_index = self.map_data.get_left_circuler_index(left_index - detect_range)
+        rstart_point_index = self.map_data.get_right_circuler_index(right_index - detect_range)
+        lstart_point = left_line[lstart_point_index]
+        rstart_point = right_line[rstart_point_index]
+
+        ## 내적을 통해 왼쪽 커브인지 오른쪽 커브인지 판단
+        sum_left = np.array([0, 0], dtype=np.float32)
+        sum_right = np.array([0, 0], dtype=np.float32)
+
+        for i in range(2*detect_range-1):
+            lnext_point_index = self.map_data.get_left_circuler_index(left_index + i - detect_range)
+            rnext_point_index = self.map_data.get_right_circuler_index(right_index + i - detect_range)
+
+            sum_left += left_line[lnext_point_index]
+            sum_right += right_line[rnext_point_index]
+        
+        sum_left_vector = sum_left - lstart_point*(2*detect_range-1)
+        sum_right_vector = sum_right - rstart_point*(2*detect_range-1)
+
+        # find end point
+        llast_point_index = self.map_data.get_left_circuler_index(left_index + detect_range)
+        rlast_point_index = self.map_data.get_right_circuler_index(right_index + detect_range)
+
+        # make standard vector
+        left_standard_vector = left_line[llast_point_index] - lstart_point
+        right_standard_vector = right_line[rlast_point_index] - rstart_point
+
+        # normalize standard vector
+        left_standard_vector /= np.linalg.norm(left_standard_vector)
+        right_standard_vector /= np.linalg.norm(right_standard_vector)
+
+        rotation_matrix = np.array([[0, -1], [1, 0]], dtype=np.float32)
+
+        dot_prod = np.dot(rotation_matrix @ left_standard_vector, sum_left_vector)\
+              + np.dot(rotation_matrix @ right_standard_vector, sum_right_vector)
+
+        if dot_prod < 0:
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    map_data = map_line_data()
+    left = map_data.get_left_line()
+    right = map_data.get_right_line()
+
+    grid_node = grid_node_map(map_data)
+
+    if grid_node.is_left_curve(0):
+        print("left curve")
+    else:
+        print("right curve")
+
+    # plot
+    plt.plot(left[:, 0], left[:, 1], 'r', label='left')
+    plt.plot(right[:, 0], right[:, 1], 'b', label='right')
+    plt.legend()
+    plt.show()

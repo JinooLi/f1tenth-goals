30f3d618242bef834dbb74560070ca69f024f745
jinooLi <jinu1423@naver.com>
2024-08-12 13:39:24 +0900

    NodeInfo 추상 클래스 추가

--- src/core/definition.py
+++ src/core/definition.py
@@ -7,3 +7,16 @@ class LineType(Enum):
     LEFT = 0
     RIGHT = 1
 
+class NodeInfo(ABC):
+    def __init__(self,
+                  node_pos_num_per_ver_line:int, 
+                  dir_num:int,
+                  speed_num:int)->np.ndarray:
+        self.node_pos_num_per_ver_line = node_pos_num_per_ver_line
+        self.dir_num = dir_num
+        self.speed_num = speed_num
+        self.node_num = node_pos_num_per_ver_line * dir_num * speed_num
+    
+    @abstractmethod
+    def make_node():
+        pass

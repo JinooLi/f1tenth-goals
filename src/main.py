cac049d3a7acf40773552019e4a668a61274ba9e
jinooLi <jinu1423@naver.com>
2024-08-19 21:51:33 +0900

    노드 테스트 코드 만드는 중.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from vertical_line_in_map import VerticalLineMap
-import matplotlib.pyplot as plt 
-from implement_defs.make_sqare_map import SquareMap
 import numpy as np
+import matplotlib.pyplot as plt 
+from vertical_line_in_map import VerticalLineMap
+from implement_defs.make_sqare_map import SquareMap
+from implement_defs.make_point_mass_node import PointMassNode
 
 if __name__ == "__main__":
     map_data = SquareMap()
@@ -14,13 +15,17 @@ if __name__ == "__main__":
 
     ver_coord_data = grid_line.get_ver_line_coord(ver_line_index)
 
-    ver_coord = ver_coord_data.coordinate
+    node_data = PointMassNode(maximum_speed=5.0)
+
+    node = node_data.get_node(ver_coord_data)
 
     # plot
     plt.plot(left[:, 0], left[:, 1], 'r', label='left')
     plt.plot(right[:, 0], right[:, 1], 'b', label='right')
+    ver_coord = ver_coord_data.coordinate
     for i in range(len(ver_coord)):
         coord = np.transpose(ver_coord[i])
         plt.plot(coord[0], coord[1], 'g')
+        # 노드의 위치 체크 요망
     plt.legend()
     plt.show()

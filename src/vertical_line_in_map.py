2097a62a24e06d9f88bbbcd9f4c423e77a0ce18d
jinooLi <jinu1423@naver.com>
2024-08-19 15:10:41 +0900

    클래스와 파일 이름 변경 및 main 추가.

--- src/vertical_line_in_map.py
+++ src/vertical_line_in_map.py
@@ -1,10 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 from core.definition import LineType, MapLineData, NodeInfo
-from implement_defs.make_sqare_map import SquareMap
 
     
-class GridNodeMap:
+class VerticalLineMap:
     def __init__(self, 
                  map_data:MapLineData, 
                  line_div_num:int = 5, 
@@ -225,25 +224,3 @@ class GridNodeMap:
             return True
         else:
             return False
-
-
-if __name__ == "__main__":
-    map_data = SquareMap()
-    left = map_data.get_left_line()
-    right = map_data.get_right_line()
-
-    grid_node = GridNodeMap(map_data,line_dist_index=400)
-
-    vir_line = grid_node.get_vir_line_index(0, 0)
-
-    # plot
-    plt.plot(left[:, 0], left[:, 1], 'r', label='left')
-    plt.plot(right[:, 0], right[:, 1], 'b', label='right')
-    for i in range(len(vir_line)):
-        left_index = vir_line[i][0]
-        right_index = vir_line[i][1]
-        left_index = map_data.get_left_mod_index(left_index)
-        right_index = map_data.get_right_mod_index(right_index)
-        plt.plot([left[left_index][0], right[right_index][0]],[left[left_index][1], right[right_index][1]], 'g')
-    plt.legend()
-    plt.show()
